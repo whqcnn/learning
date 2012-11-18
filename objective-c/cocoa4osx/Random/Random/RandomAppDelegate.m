47bdbe3189a67964a8b8b161448639d3acea7bd6
whqcnn <whqcnn@gmail.com>
2012-11-18 21:16:24 +0800

    cocoa

---
+++ objective-c/cocoa4osx/Random/Random/RandomAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  RandomAppDelegate.m
+//  Random
+//
+//  Created by whqcnn on 11/18/12.
+//  Copyright (c) 2012 whqcnn. All rights reserved.
+//
+
+#import "RandomAppDelegate.h"
+
+@implementation RandomAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ objective-c/cocoa4osx/Random/Random/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Random
+//
+//  Created by whqcnn on 11/18/12.
+//  Copyright (c) 2012 whqcnn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
