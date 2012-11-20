4e269396f4e69ab44ecc09133da46921f7ebf8d8
whqcnn <whqcnn@gmail.com>
2012-11-20 10:30:52 +0800

    一个算法题 旋转数组

---
+++ algorithm/xuanzhuan.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#define HANG 9
+#define LIE  9
+
+void print_sz(int sz[][LIE])
+{
+    int x, y;
+    for (x = 0; x < LIE; x++)
+    {
+        printf("[");
+        for (y = 0; y < HANG; y++)
+        {
+            printf("%3d", sz[x][y]);
+        }
+        printf("]\n");
+    }
+    printf("\n");
+}
+
+void xuanzhuan(int sz[][LIE])
+{
+    int i, j, n;
+    int m = 0;
+
+    for (i = 0; i < LIE/2; i++)
+    {
+        m++;
+        for (j = i; j < HANG-m; j++)
+        {
+            n = sz[i][j];
+            sz[i][j] = sz[HANG-1-j][i];
+            sz[HANG-1-j][i] = sz[HANG-1-i][LIE-1-j];
+            sz[HANG-1-i][LIE-1-j] = sz[j][LIE-1-i];
+            sz[j][LIE-1-i] = n;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int sz[HANG][LIE];
+    int i, j;
+    int n = 1;
+    for (i = 0; i < LIE; i++)
+    {
+        for (j = 0; j < HANG; j++)
+        {
+            sz[i][j] = n;
+            n++;
+        }
+    }
+    print_sz(sz);
+    xuanzhuan(sz);
+    print_sz(sz);
+}
+
